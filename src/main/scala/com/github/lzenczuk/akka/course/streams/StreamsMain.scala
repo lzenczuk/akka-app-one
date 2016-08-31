158fa06d365fc774e2189e21a6e9b4e427157d04
Lucjan Zenczuk <lucjan.zenczuk@gmail.com>
2016-08-31 12:52:32 +0100

    More experiments with streams

--- src/main/scala/com/github/lzenczuk/akka/course/streams/StreamsMain.scala
+++ src/main/scala/com/github/lzenczuk/akka/course/streams/StreamsMain.scala
@@ -3,7 +3,7 @@ package com.github.lzenczuk.akka.course.streams
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.stream.ActorMaterializer
 import akka.{Done, NotUsed}
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.scaladsl.{Flow, RunnableGraph, Sink, Source}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -27,7 +27,7 @@ class NumberProcessingActor extends Actor with ActorLogging {
       log.info(s"Receive number: $n")
     case NumberProcessingActor.StreamCompleted =>
       log.info("Stream completed. Terminating actor system")
-      context.system.terminate()
+      //context.system.terminate()
   }
 }
 
@@ -36,8 +36,8 @@ object StreamsMain extends App{
   implicit private val materializer: ActorMaterializer = ActorMaterializer()
 
   private val source: Source[Int, NotUsed] = Source(1 to 100)
-  private val addTen = Flow[Int].map(_+10)
-  private val subTen = Flow[Int].map(_-10)
+  private val addTen: Flow[Int, Int, NotUsed] = Flow[Int].map(_+10)
+  private val subTen: Flow[Int, Int, NotUsed] = Flow[Int].map(_-10)
   // private val sink: Sink[Int, Future[Done]] = Sink.foreach[Int](println)
   private val actorSink: Sink[Any, NotUsed] = Sink.actorRef(system.actorOf(Props[NumberProcessingActor]), NumberProcessingActor.StreamCompleted)
 
@@ -45,9 +45,13 @@ object StreamsMain extends App{
   source.via(addTen).via(subTen).runWith(actorSink)
   println("Main ended")
 
-  /*source.via(addTen).via(subTen).runWith(sink).andThen{
-    case _ =>
-      println("End")
-      system.terminate()
-  }*/
+  private val runnableGraph: RunnableGraph[NotUsed] = Source(1 to 10)
+    .map(_+2)
+    .map(_-1)
+    .alsoTo(Sink.foreach(n => println(s"Stream 1: $n")))
+    .to(Sink.foreach(n => println(s"Stream 2: $n")))
+  runnableGraph.run()
+
+  Thread.sleep(1000L)
+  system.terminate()
 }
